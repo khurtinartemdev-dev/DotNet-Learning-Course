8352fa0ad305eb091b5dd8db0d27c3a4fce1051f
khurt <khurt@-_->
2026-09-07 20:51:58 +0200

    Add project files.

---
+++ Lesson01_Basics/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Lesson01_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Name: ");
+            string namePlayer = Console.ReadLine();
+            Console.Write("Class: ");
+            string classPlayer = Console.ReadLine();
+            Console.Write("Level: ");
+            double levelPlayer = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Health: ");
+            double healthPlayer = Convert.ToDouble(Console.ReadLine());
+
+            double maxHealthPlayer = healthPlayer * 1.2d;
+
+            Console.WriteLine($"""
+
+                Name: {namePlayer}
+                Class: {classPlayer}
+                Level: { levelPlayer}
+                Health: {healthPlayer}
+                Max health: {maxHealthPlayer}
+
+                """);
+
+            
+        }
+    }
+}
