41117e19dd938d29eb95dbe669c4cb8ad9953dd8
khurtinartemdev-dev <khurtin.artem.dev@gmail.com>
2026-09-08 20:29:09 +0200

    feat: implement basic Enemy class with fields and TakeDamage method

---
+++ Lesson08_Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lesson08_Classes
+{
+    class Enemy
+    {
+        public string Type = "";
+        public int Health;
+        public int BaseDamage;
+        public void TakeDamage(int amount)
+        {
+            Health -= amount;
+            Console.WriteLine($"{Type} took {amount} damage! Remaining HP: {Health}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy goblin = new Enemy();
+
+            goblin.Type = "Goblin";
+            goblin.Health = 50;
+            goblin.BaseDamage = 10;
+
+            goblin.TakeDamage(20);
+
+        }
+    }
+}
