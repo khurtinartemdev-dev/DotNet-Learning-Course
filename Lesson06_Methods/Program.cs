93b3149cb96a95c23cd9cf1e33255e89ea468938
khurtinartemdev-dev <khurtin.artem.dev@gmail.com>
2026-09-08 19:33:43 +0200

    feat: implement modular combat methods and ternary logic

---
+++ Lesson06_Methods/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lesson06_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintHeader("COMBAT LOG");
+
+            int totalDamage = CalculateFinalDamage(50, 20);
+            Console.WriteLine($"Total damage: {totalDamage}");
+
+            if (IsPlayerAlive(100 - totalDamage)) 
+            {
+                Console.WriteLine("The player is alive");
+            }
+            else 
+            {
+                Console.WriteLine("The player died");
+            }  
+
+        }
+        static void PrintHeader(string title)
+        {
+            Console.WriteLine($"=== {title} ===");
+        }
+        static int CalculateFinalDamage(int baseDamage, int armor)
+        {
+            return (baseDamage <= armor) ? 0 : baseDamage - armor;
+        }
+        static bool IsPlayerAlive(int health) 
+        { 
+            return health > 0;
+        }
+        
+
+    }
+}
