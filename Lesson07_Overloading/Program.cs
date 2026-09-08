8b02443f2420aed1d6d7575f8447e08cefd959a3
khurtinartemdev-dev <khurtin.artem.dev@gmail.com>
2026-09-08 20:16:53 +0200

    feat: implement method overloading and optional parameters

---
+++ Lesson07_Overloading/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Lesson07_Overloading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Attack(20);
+            Attack(20, "critical");
+            Heal(30);
+            Heal(50, "Greater Potion");
+        }
+        static void Attack(int baseDamage)
+        {
+            Console.WriteLine($"Dealt {baseDamage} damage!");
+        }
+        static void Attack(int baseDamage, string criticalType)
+        {
+            if(criticalType.ToLower() == "critical")
+            {
+                baseDamage *= 2;
+                Console.WriteLine($"CRITICAL HIT! Dealt {baseDamage} damage!");
+
+            }
+            else
+            {
+                Attack(baseDamage);
+            }
+        }
+        static void Heal(int amount, string potionName = "Standard Potion")
+        {
+            Console.WriteLine($"Healed for {amount} HP using {potionName}.");
+        }
+    }
+}
