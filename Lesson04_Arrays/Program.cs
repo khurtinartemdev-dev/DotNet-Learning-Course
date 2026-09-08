57fc02e7a7dbc5e618b48ae141036d9adf2d6a61
khurtinartemdev-dev <khurtin.artem.dev@gmail.com>
2026-09-08 18:59:19 +0200

    feat: implement inventory array and precise average damage calculation

---
+++ Lesson04_Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Lesson04_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inventory = { "Iron Sword", "Health Potion", "Wooden Shield", "Magic Ring" };
+
+            foreach (var item in inventory)
+            { 
+                Console.WriteLine(item);
+            }
+
+            int[] damages = { 15, 25, 10, 30, 5 };
+            int sumDamage = 0;
+            foreach (var damage in damages)
+            {
+                sumDamage += damage;
+            }
+            double averageDamage = (double)sumDamage/damages.Length;
+
+            Console.WriteLine($"Total damage: {sumDamage}");
+            Console.WriteLine($"Average damage: {averageDamage}");
+
+        }
+    }
+}
