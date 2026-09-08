12981a72725733a834e4c7a2b2989be94cc77b39
khurtinartemdev-dev <khurtin.artem.dev@gmail.com>
2026-09-08 19:11:13 +0200

    feat: implement quest log with updating list using List<string>

---
+++ Lesson05_Lists/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Lesson05_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> activeQuests = new List<string>();
+
+            activeQuests.Add("Collect wood");
+            activeQuests.Add("Create a sword");
+            activeQuests.Add("Defeat the dragon");
+
+            Console.WriteLine(activeQuests.Count);
+            foreach (var Quest in activeQuests)
+            {
+                Console.WriteLine(Quest);
+            }
+
+            activeQuests.Remove("Defeat the dragon");
+
+            if(activeQuests.Contains("Defeat the dragon"))
+            {
+                Console.WriteLine("The quest was not deleted.");
+            }
+            else
+            {
+                Console.WriteLine("The quest has been removed");
+            }
+
+            Console.WriteLine("\nUpdated Quest Log:");
+            foreach (var quest in activeQuests)
+            {
+                Console.WriteLine(quest);
+            }
+        }
+    }
+}
