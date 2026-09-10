22e5c61a8a6f33f23b1e25cfb45fdf41f87b73f6
khurtinartemdev-dev <khurtin.artem.dev@gmail.com>
2026-09-10 17:21:38 +0200

    feat: implement Entity base class and Hero derived class with inheritance

---
+++ Lesson11_Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Lesson11_Inheritance
+{
+    class Entity
+    {
+        public string Name { get; set; }
+        public int Health { get; protected set; }
+
+        public Entity(string name, int health)
+        {
+            this.Name = name;
+            this.Health = health;
+        }
+
+        public void TakeDamage(int amount)
+        {
+            this.Health = Math.Max(0, this.Health - amount);
+            Console.WriteLine($"{this.Name} received damage: {amount} Your health: {this.Health}");
+        }
+    }
+
+    class Hero : Entity
+    {
+        public string ClassName { get; set; }
+
+        public Hero(string name, int health, string className) : base(name, health)
+        {
+            this.ClassName = className;
+        }
+
+        public void UseAbility()
+        {
+            Console.WriteLine($"{this.Name} uses class ability as {this.ClassName}!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero knight = new Hero("Arthur", 120, "Paladin");
+            knight.TakeDamage(30);
+            knight.UseAbility();
+        }
+    }
+}
