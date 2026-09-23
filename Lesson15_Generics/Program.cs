a5cf55b80cc3adc5c5b3f2e31e511903f89d072d
khurtinartemdev-dev <khurtin.artem.dev@gmail.com>
2026-09-23 17:06:59 +0200

    Lesson 15: Implement Generic Storage<T>

---
+++ Lesson15_Generics/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Lesson15_Generics
+{
+    public class Storage<T>
+    {
+        private List<T> _items = new List<T>();
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public T GetItem(int index) 
+        {
+            return _items[index];
+        }
+
+        public void PrintAll()
+        {
+            foreach (var item in _items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Storage<int> numbers = new Storage<int>();
+            Storage<string> strings = new Storage<string>();
+
+            numbers.Add(1);
+            numbers.Add(3);
+            numbers.Add(10);
+            numbers.Add(13);
+
+            strings.Add("One");
+            strings.Add("Two");
+            strings.Add("Three");
+
+            numbers.PrintAll();
+            strings.PrintAll();
+
+        }
+    }
+}
